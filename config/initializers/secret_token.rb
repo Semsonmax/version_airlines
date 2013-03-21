d81388ebe8339a3ed6ae0021a49fff3d8ebe86d1
Sandip Trivedi <trivedi.sandip@gmail.com>
2013-03-21 16:12:26 -0400

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VersionAirlines::Application.config.secret_token = '3adb0c2308379d3e19c9022deccd35ed80288ef8d996eac9258d80c91a04b74980df8fcd577a593968c78d6e6268fb2acb0585e01fc2e1396a3a6a18c7518a01'
