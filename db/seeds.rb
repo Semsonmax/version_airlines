59d8fbaa48cac3ae9c83fb16c9eedc86937a073c
Sandip Trivedi <trivedi.sandip@gmail.com>
2013-03-22 19:12:22 -0400

    Trying to create seat plans

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,12 +7,8 @@ z1 = Zeppelin.create(name: 'The Lord Grantham', style: 'Victoria Class', number_
 z2 = Zeppelin.create(name: 'The Admiral Nelson', style: 'Wellington Class', number_of_rows: 30, number_of_columns: 6)
 z3 = Zeppelin.create(name: 'The Royal Ulsterman', style: 'Clipper Class', number_of_rows: 15, number_of_columns: 4)
 z4 = Zeppelin.create(name: 'HMS Buttercup', style: 'Vitoria Class', number_of_rows: 40, number_of_columns: 9)
-z1.create_seats
-z2.create_seats
-z3.create_seats
-z4.create_seats
-v1 = Voyage.create(zeppelin_id: z1.id)
-v2 = Voyage.create(zeppelin_id: z2.id)
+z5 = Zeppelin.create(name: 'Test', style: 'Test', number_of_rows: 3, number_of_columns: 3)
+v1 = Voyage.create(zeppelin_id: z5.id)
 t1 = Traveler.create(name: 'Winthrop', email: 'winthrop@gmail.com', password: 'a', password_confirmation: 'a')
 a1 = Traveler.create(name: 'admin', email: 'admin@gmail.com', password: 'a', password_confirmation: 'a')
 a1.is_admin = true
