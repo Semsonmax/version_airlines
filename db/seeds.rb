a18b77ca277d05830fa62807d5a12e5fb2159295
Ron Jones <rrrron@gmail.com>
2013-03-22 14:47:37 -0400

    admin to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,7 @@ v1.zeppelin = z1
 t1 = Traveler.create
 t2 = Traveler.create
 t3 = Traveler.create(name: 'Winthrop', email: 'winthrop@gmail.com', password: 'a', password_confirmation: 'a')
+a1 = Traveler.create(name: 'admin', email: 'admin@gmail.com', password: 'a', password_confirmation: 'a')
+a1.is_admin = true
+a1.save
+
