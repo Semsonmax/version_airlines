88039db0725ae17b9c8b6985f23a1d34328525f6
Brian Yingling <brian_yingling@yahoo.com>
2013-03-22 13:32:06 -0400

    writing voyage_spec.rb

--- spec/models/voyage_spec.rb
+++ spec/models/voyage_spec.rb
@@ -1,10 +1,22 @@
-<<<<<<< HEAD
+# == Schema Information
+#
+# Table name: voyages
+#
+#  id              :integer          not null, primary key
+#  code            :string(255)
+#  airfield_depart :string(255)
+#  airfield_arrive :string(255)
+#  voyage_date     :date
+#  zeppelin_id     :integer
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 require 'spec_helper'
 
 
 describe Voyage do
   let(:voy) {FactoryGirl.create(:voyage)}
-  let(:book) {FactoryGirl.create(:booking)}
   let(:trvlr) {FactoryGirl.create(:traveler)}
   let(:seat) {FactoryGirl.create(:seat)}
   let(:zep) {FactoryGirl.create(:zeppelin)}
@@ -32,19 +44,3 @@ describe Voyage do
     end
   end
 end
-=======
-# == Schema Information
-#
-# Table name: voyages
-#
-#  id              :integer          not null, primary key
-#  code            :string(255)
-#  airfield_depart :string(255)
-#  airfield_arrive :string(255)
-#  voyage_date     :date
-#  zeppelin_id     :integer
-#  created_at      :datetime         not null
-#  updated_at      :datetime         not null
-#
-
->>>>>>> 066192cf0c3d43713717dac69a4e567b09c4e56d
