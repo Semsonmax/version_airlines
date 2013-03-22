5fe60d7c70f6b2848661394d10fc049f3eec6237
Tony Contreras <tony.contreras@gmail.com>
2013-03-22 18:10:34 -0400

    zeppelin forms

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,8 @@ class ApplicationController < ActionController::Base
   def authenticate
     @auth = (session[:traveler_id].present?) ? Traveler.find(session[:traveler_id]) : nil
   end
+
+  def check_if_admin
+    redirect_to(root_path) if @auth.nil? || !@auth.is_admin
+  end
 end
