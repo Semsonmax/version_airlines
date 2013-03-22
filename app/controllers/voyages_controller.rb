a7b6d543f81abcb7fca9e5a6d0f786862dabd432
Brian Yingling <brian_yingling@yahoo.com>
2013-03-22 18:09:44 -0400

    added new voyage form to voyages index page

--- app/controllers/voyages_controller.rb
+++ app/controllers/voyages_controller.rb
@@ -1,4 +1,9 @@
 class VoyagesController < ApplicationController
   def index
+    @voyage = Voyage.new
+  end
+  def new
+    binding.pry
+    @voyage = Voyage.new
   end
 end
--- spec/features/voyages_spec.rb
+++ spec/features/voyages_spec.rb
@@ -1,15 +1,28 @@
-# require 'spec_helper'
-# require 'features/shared/login_helper'
-# include LoginHelper
+require 'spec_helper'
+require 'features/shared/login_helper'
+include LoginHelper
 
-# describe 'Voyages' do
-#   before(:each) do
-#     admin = FactoryGirl.create(:admin_traveler)
-#     login_to_system(admin)
-#   end
-#   describe 'GET /voyages' do
-#     it 'displays the New Voyage button' do
-#       click_link('New Voyage')
-#     end
-#   end
-# end
+describe 'Voyages' do
+  before(:each) do
+    visit root_path
+    admin = FactoryGirl.create(:admin_traveler)
+    login_to_system(admin)
+  end
+
+  describe 'GET /voyages' do
+    it 'displays the New Voyage button', :js=>true do
+      click_link('Voyages')
+      click_link('New Voyage')
+    end
+  end
+
+  describe 'GET /voyages/new' do
+    it 'displays the New Voyage form', :js=>true do
+      click_link('Voyages')
+      click_link('New Voyage')
+      page.should have_button('Create Voyage')
+    end
+  end
+
+
+end
