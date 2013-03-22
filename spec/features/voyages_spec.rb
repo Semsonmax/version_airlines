cc164bd552642bed7b16988326c07ad6855b4857
Brian Yingling <brian_yingling@yahoo.com>
2013-03-22 15:12:46 -0400

    working on voyages_spec.rb

--- spec/features/voyages_spec.rb
+++ spec/features/voyages_spec.rb
@@ -1,5 +1,15 @@
-require 'spec_helper'
+# require 'spec_helper'
+# require 'features/shared/login_helper'
+# include LoginHelper
 
-describe 'Voyages' do
-
-end
+# describe 'Voyages' do
+#   before(:each) do
+#     admin = FactoryGirl.create(:admin_traveler)
+#     login_to_system(admin)
+#   end
+#   describe 'GET /voyages' do
+#     it 'displays the New Voyage button' do
+#       click_link('New Voyage')
+#     end
+#   end
+# end
