9e7c18ef98b1a2eb6ab9997b225552d795fdf167
Ron Jones <rrrron@gmail.com>
2013-03-22 14:29:44 -0400

    added register button for non-auth users

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,9 @@ module ApplicationHelper
     if @auth.present?
       nav += "<li>#{link_to('Logout', '/login', :method => :delete, :remote => true, :class => 'button tiny alert')}</li>"
     else
-      nav += "<li>#{link_to('Login', '/login', :remote => true, :class => 'button tiny success')}</li>"
+      nav += "<li>#{link_to('Register', '/traveler/new', :remote => true, :class => 'button tiny')}</li>"
+      nav += "<li class='divider'></li>"
+      nav += "<li>#{link_to('Login', '/login', :remote => true, :class => 'button tiny')}</li>"
     end
     nav
   end
