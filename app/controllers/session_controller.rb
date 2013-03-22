95f247fa6a5052f440ce0c0ef991f946ae1b254a
Ron Jones <rrrron@gmail.com>
2013-03-22 12:33:56 -0400

    pulling

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -5,16 +5,16 @@ class SessionController < ApplicationController
   end
 
   def create
-    @auth = User.where(:email => params[:email]).first
+    @auth = Traveler.where(:email => params[:email]).first
     if @auth.present? && @auth.authenticate(params[:password])
-      session[:user_id] = @auth.id
+      session[:traveler_id] = @auth.id
     else
-      session[:user_id] = nil
+      session[:traveler_id] = nil
     end
   end
 
   def destroy
-    session[:user_id] = nil
+    session[:traveler_id] = nil
   end
 
 end
