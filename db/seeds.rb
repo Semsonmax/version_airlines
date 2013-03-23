e97772d5839c936e600c4723314f64d0ac2a4df2
Tony Contreras <tony.contreras@gmail.com>
2013-03-23 08:19:58 -0400

    workin on the zeppelin seat chart for admins

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,11 +8,9 @@ z1 = Zeppelin.create(name: 'The Lord Grantham', style: 'Victoria Class', number_
 z2 = Zeppelin.create(name: 'The Admiral Nelson', style: 'Wellington Class', number_of_rows: 30, number_of_columns: 6)
 z3 = Zeppelin.create(name: 'The Royal Ulsterman', style: 'Clipper Class', number_of_rows: 15, number_of_columns: 4)
 z4 = Zeppelin.create(name: 'HMS Buttercup', style: 'Vitoria Class', number_of_rows: 40, number_of_columns: 9)
-<<<<<<< HEAD
 z5 = Zeppelin.create(name: 'Test', style: 'Test', number_of_rows: 3, number_of_columns: 3)
 v1 = Voyage.create(zeppelin_id: z5.id)
 t1 = Traveler.create(name: 'Winthrop', email: 'winthrop@gmail.com', password: 'a', password_confirmation: 'a')
-=======
 
 v1 = Voyage.create(code: 'BN0401', airfield_depart: 'Berlin', airfield_arrive: 'New York', voyage_date: '1900-04-01')
 v2 = Voyage.create(code: 'LP0501', airfield_depart: 'London', airfield_arrive: 'Paris', voyage_date: '1900-05-01')
@@ -29,7 +27,6 @@ t1 = Traveler.create(name: 'Winthrop', email: 'winthrop@gmail.com', password: 'a
 t2 = Traveler.create(name: 'Reginald', email: 'reg@gmail.com', password: 'a', password_confirmation: 'a')
 t3 = Traveler.create(name: 'Bartholomew', email: 'bart@gmail.com', password: 'a', password_confirmation: 'a')
 
->>>>>>> ab619c638cfe06a32a046b0a42d344a39eeca6e5
 a1 = Traveler.create(name: 'admin', email: 'admin@gmail.com', password: 'a', password_confirmation: 'a')
 a1.is_admin = true
 a1.save
