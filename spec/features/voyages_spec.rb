9ddef8fcfc772acd536b8113fa99c3fbb2d748e9
Brian Yingling <brian_yingling@yahoo.com>
2013-03-22 22:36:52 -0400

    still working on voyages_spec.rb

--- spec/features/voyages_spec.rb
+++ spec/features/voyages_spec.rb
@@ -3,19 +3,19 @@ require 'features/shared/login_helper'
 include LoginHelper
 
 describe 'Voyages' do
-  #let(:admin) {FactoryGirl.create(:admin_traveler)}
+  let(:admin) {FactoryGirl.create(:admin_traveler)}
   let(:voyage) {FactoryGirl.create(:voyage)}
   let(:zep) {FactoryGirl.create(:zeppelin)}
   before(:each) do
     voyage.zeppelin = zep
     voyage.save
     visit root_path
-    admin = FactoryGirl.create(:admin_traveler)
-    login_to_system(admin)
+    #admin = FactoryGirl.create(:admin_traveler)
   end
 
   describe 'GET /voyages' do
     it 'displays the New Voyage button', :js=>true do
+      login_to_system(admin)
       click_link('Voyages')
       click_link('New Voyage')
     end
@@ -23,25 +23,26 @@ describe 'Voyages' do
 
   describe 'GET /voyages/new' do
     it 'displays the New Voyage form', :js=>true do
-      click_link('Voyages')
-      click_link('New Voyage')
-      page.should have_button('Create Voyage')
+      login_to_system(admin)
+      visit voyages_path
+      page.should have_link('New Voyage')
     end
   end
 
-  describe 'GET /voyages/create' do
-    it 'creates a Voyage in the db', :js=>true do
-      click_link('Voyages')
-      click_link('New Voyage')
-      fill_in('Code', :with=>voyage.code)
-      fill_in('voyage_airfield_depart', :with=>voyage.airfield_depart)
-      fill_in('voyage_airfield_arrive', :with=>voyage.airfield_arrive)
-      fill_in('voyage_voyage_date', :with=>voyage.voyage_date)
-      select(zep.style, :from=>'voyage_zeppelin_id')
-      click_button('Create Voyage')
-      expect(voyage.id).to_not eq nil
-    end
-  end
+  # describe 'POST /voyages' do
+  #   it 'creates a Voyage in the db', :js=>true do
+  #     login_to_system(admin)
+  #     click_link('Voyages')
+  #     click_link('New Voyage')
+  #     fill_in('Code', :with=>voyage.code)
+  #     fill_in('voyage_airfield_depart', :with=>voyage.airfield_depart)
+  #     fill_in('voyage_airfield_arrive', :with=>voyage.airfield_arrive)
+  #     fill_in('voyage_voyage_date', :with=>voyage.voyage_date)
+  #     select(zep.style, :from=>'voyage_zeppelin_id')
+  #     click_button('Create Voyage')
+  #     expect(voyage.id).to_not eq nil
+  #   end
+  # end
 
 
 end
