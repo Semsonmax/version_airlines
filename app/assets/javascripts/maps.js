130d445d29a91f6090155380e2146e343f088e94
Brian Yingling <brian_yingling@yahoo.com>
2013-03-24 12:37:46 -0400

    working on bounds in google maps

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -4,16 +4,21 @@ var map;
 
 function display_map(lat, lon, zoom, canvas) {
   canvas = canvas[0];
+  latlng = new google.maps.LatLng(lat, lon);
+  bounds = new google.maps.LatLngBounds();
+
   var mapOptions = {
-    center: new google.maps.LatLng(lat, lon),
+    center: latlng,
     zoom: zoom,
     mapTypeId: google.maps.MapTypeId.ROADMAP
   };
 
   map = new google.maps.Map(canvas, mapOptions);
-
+  // map.fitBounds(new google.maps.LatLngBounds(
+  //     new google.maps.LatLng(lat / 2, lon / 2),
+  //     new google.maps.LatLng(lat, lon)
+  // ));
 }
-
 
 
 function add_marker(lat, lon, title, canvas) {
@@ -21,3 +26,19 @@ function add_marker(lat, lon, title, canvas) {
   var marker = new google.maps.Marker({position: coords, map: map, title: title});
   marker.setMap(map);
 }
+
+function set_bounds() {
+
+}
+
+
+// function setBounds() {
+// // map: an instance of GMap2
+// // latlng: an array of instances of GLatLng
+// var latlngbounds = new GLatLngBounds();
+// for (var i = 0; i < latlng.length; i++)
+// {
+//   latlngbounds.extend(latlng[i]);
+// }
+// map.setCenter(latlngbounds.getCenter(), map.getBoundsZoomLevel(latlngbounds));
+// }
