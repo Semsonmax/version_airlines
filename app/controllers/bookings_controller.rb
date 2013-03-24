ffa33cb559eb52af1725023d8302780c5e1f84df
Ron Jones <rrrron@gmail.com>
2013-03-24 12:49:31 -0400

    pulling

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,6 +1,5 @@
 class BookingsController < ApplicationController
   def create
-    binding.pry
     Booking.create(seat_id: params[:seat_id], traveler_id: params[:traveler_id], voyage_id: params[:voyage_id])
     @voyage = Voyage.find(params[:voyage_id])
     seat = Seat.find(params[:seat_id])
