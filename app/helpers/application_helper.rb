8bbbe7d45e6d7c8e8aeda790cd0eb0856bd8896a
Sandip Trivedi <trivedi.sandip@gmail.com>
2013-03-24 21:11:54 -0400

    More fixes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,35 +5,23 @@ module ApplicationHelper
       nav += "<li>#{link_to('Logout', '/login', :method => :delete, :remote => true, :class => 'button tiny alert')}</li>"
       if @auth.is_admin == true
         nav += "<li class='divider'></li>"
-        nav += "<li class='has-dropdown'>#{link_to('Zeppelins', zeppelins_path, :class => 'button tiny')}"
-        nav += "<ul class='dropdown'>"
-        nav += "<li>#{link_to('Create New Zeppelin', new_zeppelin_path)}</li>"
-        @zeppelins.each do |zeppelin|
-          nav += "<li>#{link_to(zeppelin.name, '#')}</li>"
-        end
-        nav += "</ul>"
-        nav += "</li>"
+        nav += "<li>#{link_to('Zeppelins', zeppelins_path, :class => 'button tiny')}</li>"
         nav += "<li class='divider'></li>"
-        nav += "<li class='has-dropdown'>#{link_to('Voyages', voyages_path, :class => 'button tiny')}"
-        nav += "<ul class='dropdown'>"
-        nav += "<li>#{link_to('Create New Voyage', new_voyage_path)}</li>"
-        @voyages.each do |voyage|
-          nav += "<li>#{link_to(voyage.code, voyage)}</li>"
-        end
-        nav += "</ul>"
-        nav += "</li>"
+        nav += "<li>#{link_to('Voyages', voyages_path, :class => 'button tiny')}</li>"
         nav += "<li class='divider'></li>"
       else
         nav += "<li class='divider'></li>"
-        # nav += "<li>#{link_to('My Voyages', '#', :class => 'button tiny')}</li>"
-        # nav += "<li class='divider'></li>"
-        nav += "<li class='has-dropdown'>#{link_to('Find Voyages', voyages_path, :class => 'button tiny')}"
+        nav += "<li class='has-dropdown'>#{link_to('My Voyages', '#', :class => 'button tiny')}"
         nav += "<ul class='dropdown'>"
-        @voyages.each do |voyage|
+        @auth.voyages.uniq.each do |voyage|
           nav += "<li>#{link_to(voyage.code, voyage)}</li>"
         end
         nav += "</ul>"
         nav += "</li>"
+        nav += "<li class='divider'></li>"
+        nav += "<li>#{link_to('Find Voyages', voyages_path, :class => 'button tiny')}</li>"
+        nav += "<li class='divider'></li>"
+        nav += "<li>#{link_to('My Account', '#', :class => 'button tiny')}</li>"
         nav += "<li class='divider'></li>"
       end
     else
