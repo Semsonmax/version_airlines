ab5090fb55080e580a5fced5179214427268cfc1
Tony Contreras <tony.contreras@gmail.com>
2013-03-25 01:37:03 -0400

    done

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,6 +3,6 @@ class BookingsController < ApplicationController
     Booking.create(seat_id: params[:seat_id], traveler_id: params[:traveler_id], voyage_id: params[:voyage_id])
     @voyage = Voyage.find(params[:voyage_id])
     seat = Seat.find(params[:seat_id])
-    flash[:message] = "CONFIRMATION: You have booked seat #{seat.row} - #{seat.convert_column_to_letter} on the voyage #{@voyage.code}"
+    flash[:message] = "CONFIRMATION: You have booked seat #{seat.convert_column_to_letter} - #{seat.row}on the voyage #{@voyage.code}"
   end
 end
