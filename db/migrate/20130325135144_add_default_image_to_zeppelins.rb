f2b166f296a155e4a779cc5ab89c33c39aa9041e
Tony Contreras <tony.contreras@gmail.com>
2013-03-25 11:22:24 -0400

    added default image to new zeppelin create in DB

--- db/migrate/20130325135144_add_default_image_to_zeppelins.rb
+++ db/migrate/20130325135144_add_default_image_to_zeppelins.rb
@@ -1,4 +1,5 @@
 class AddDefaultImageToZeppelins < ActiveRecord::Migration
   def change
+    change_column :zeppelins, :image, :string, :default => 'zep1.jpg'
   end
 end
--- spec/models/zeppelin_spec.rb
+++ spec/models/zeppelin_spec.rb
@@ -10,10 +10,11 @@
 #  updated_at        :datetime         not null
 #  name              :string(255)
 #  description       :text
-#  image             :string(255)
+#  image             :string(255)      default("zep1.jpg")
 #  engines           :text
 #  year_built        :integer
 #
+
 
 require 'spec_helper'
 
